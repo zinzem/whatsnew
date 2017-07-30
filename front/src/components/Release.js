6b18d3549c2db7bc61e9ca10c818fa97dfd1b14d
Ruben Renzema <ruben.renzema@gmail.com>
2017-07-30 14:22:02 +0200

    add release date + github markdown css

--- front/src/components/Release.js
+++ front/src/components/Release.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import Markdown from'react-remarkable';
+
 import './Release.css';
+
+const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wedesday", "Thurday", "Friday", "Saturday"];
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 class Release extends Component {
 
   constructor(props) {
     super(props);
     this.title = props.data.title[0];
+    this.updatedDate = new Date(Date.parse(props.data.updated[0]));
+    this.updatedDateString = daysOfTheWeek[this.updatedDate.getDay()]
+      + " " + this.updatedDate.getDate()
+      + " " + months[this.updatedDate.getMonth()]
+      + " " + this.updatedDate.getFullYear();
     this.changelog = props.data.content[0]._;
 }
 
@@ -13,7 +23,9 @@ class Release extends Component {
     return (
       <div className="release">
         <h2>{this.title}</h2>
+        <p>{this.updatedDateString}</p>
         <div
+        className="markdown"
           dangerouslySetInnerHTML={{ __html: this.changelog }}
         />
       </div>
--- front/src/components/Releases.js
+++ front/src/components/Releases.js
@@ -3,10 +3,12 @@ import Release from './Release';
 import Rx from 'rxjs';
 import superagent from 'superagent';
 
+import './Releases.css';
+
 let api = {
   getFeed(username, repo, token) {
     return Rx.Observable.create(observer => {
-      superagent.get('/feed/'+ username + '/' + repo + (token ? ('?token=' + token) : ''))
+      superagent.get('https://whats-new.herokuapp.com/feed/'+ username + '/' + repo + (token ? ('?token=' + token) : ''))
       .accept('json')
       .end(function (error, response) {
         if (error) {
@@ -25,12 +27,12 @@ class Releases extends Component {
   constructor(props) {
     super(props);
     this.state = {releases: []};
-    let username = props.match.params.username;
-    let repo = props.match.params.repo;
+    this.username = props.match.params.username;
+    this.repo = props.match.params.repo;
     let query = new URLSearchParams(props.location.search)
     let token = query.get('token');
     
-    api.getFeed(username, repo, token).subscribe((feed) => {
+    api.getFeed(this.username, this.repo, token).subscribe((feed) => {
       let releases = feed.entry.map((entry) => <li key={entry.id}><Release data={entry}/></li>);
       this.setState({releases: releases});
     }, (error) => {
@@ -40,7 +42,10 @@ class Releases extends Component {
 
   render() {
     return (
-      <ul>{this.state.releases}</ul>
+      <div>
+        <h1>Changelog of {this.username}'s {this.repo}</h1>
+        <ul>{this.state.releases}</ul>
+      </div>
     );
   }
 }
